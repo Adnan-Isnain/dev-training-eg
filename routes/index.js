c261d134cc7e9a516dc1cd6c6be10b05c0fbc359
Adnan-Isnain <69179179+Adnan-Isnain@users.noreply.github.com>
2022-06-17 14:32:35 +0700

    Adding post route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+let counter = 0;
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Hello guys!!!' });
+  res.render('index', { title: 'Hello guys!!!', counter: counter });
+});
+
+/* POST increase counter by 1. */
+router.post("/increase-counter", function (req, res, next) {
+  counter = counter + 1;
+  res.redirect("/");
 });
 
 module.exports = router;
